0194ebae2076978f19b3134920feccd788d87639
Joe Crespo <joe@aten.io>
2016-02-29 13:20:02 -0700

    create dist for gh pages

--- README.md
+++
@@ -1,6 +0,0 @@
-## BADCamp presentation: Old Dogs, New Tricks: Supporting Websites After Launch
-
-The sweetest Reveal.js generator ever:
-
-https://github.com/slara/generator-reveal
-
--- dist/js/main.js
+++
@@ -1,8 +0,0 @@
-Reveal.addEventListener( 'slidechanged', function( event ) {
-    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-    var $currentSlide = jQuery(event.currentSlide);
-
-    if ($currentSlide.attr('data-html') === 'slides/82-casestudy-1-1.html') {
-    }
-});
-
