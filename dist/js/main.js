a80cd3197f8e5fe0fced3f8534509cb10a5d0ee1
Joe Crespo <joe@aten.io>
2016-02-29 08:46:09 -0700

    minor change to main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,8 @@
 Reveal.addEventListener( 'slidechanged', function( event ) {
     // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-    var $currentSlide = $(event.currentSlide);
+    var $currentSlide = jQuery(event.currentSlide);
 
     if ($currentSlide.attr('data-html') === 'slides/82-casestudy-1-1.html') {
     }
 });
+
