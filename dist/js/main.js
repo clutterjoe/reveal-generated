fcb0c5e2fc91eb479069747d6aea8c51fb55e1cc
Joe Crespo <joe@atendesigngroup.com>
2016-02-29 07:40:57 -0700

    add dist to repo

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+Reveal.addEventListener( 'slidechanged', function( event ) {
+    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+    var $currentSlide = $(event.currentSlide);
+
+    if ($currentSlide.attr('data-html') === 'slides/82-casestudy-1-1.html') {
+    }
+});
---
+++ dist/slides/index.md
@@ -0,0 +1,10 @@
+
+# Aten Consulting and Production Support
+
+From the terminal, pop in:
+
+  ```yo reveal:slide "Slide Title"```
+
+Available options:
+
+ ```--markdown --attributes --notes```
